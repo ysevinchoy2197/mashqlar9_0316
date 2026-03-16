6dd0b3d523a37a3cdff954793c548304a21786df
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-03-16 16:28:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+#2-misol
+def yosh(y):
+    return y
+
+res = yosh(20)
+print(res)
+
+#3-misol
+def summa(a,b):
+    return a,b
+
+res = summa(5,6)
+print(res)
+
+#4-misol
+def kopaytir(a,b):
+    return a * b
+
+res = kopaytir(3, 4)
+print(res)
+
+#1-misol
+def summa(a,b):
+    return a+b
+
+res = summa(100, 200)
+print(res)
+
+#2-misol
+def kvadrat(x):
+    return x*x
+
+res = kvadrat(5)
+print(res)
+
+#3-misol
+def salom():
+    print("Hello")
+salom()
+
+#4-misol
+def minus(a,b):
+    return a-b
+
+res = minus(20, 10)
+print(res)
+
+#5-misol
+def kub(x):
+    return x**3
+
+res = kub(3)
+print(res)
